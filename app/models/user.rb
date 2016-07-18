ea26fcac9c8e281d1fa36ee18fa090793fe7ff8f
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-07-18 19:46:00 -0400

    nil to_s

--- app/models/user.rb
+++ app/models/user.rb
@@ -107,7 +107,7 @@ class User < ActiveRecord::Base
 
   # task handling
   def get_ordered_tasks!(tag_str = nil)
-    cached_ordered_tasks = $redis.get(self.redis_key + "tag:" + tag_str)
+    cached_ordered_tasks = $redis.get(self.redis_key + "tag:" + tag_str.to_s)
     if cached_ordered_tasks.nil?
       generate_ordered_tasks!(tag_str)
     end
@@ -122,7 +122,7 @@ class User < ActiveRecord::Base
     sorted_tasks.sort_by do |task|
       task.generate_importance! + Task.random_score
     end.reverse
-    $redis.set(self.redis_key + "tag:" + tag_str, sorted_tasks);
+    $redis.set(self.redis_key + "tag:" + tag_str.to_s, sorted_tasks);
     return sorted_tasks
   end
 
