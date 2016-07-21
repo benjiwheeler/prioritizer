b636aeed05578989d90c8930e6b063b4ef478dad
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-07-20 20:17:53 -0400

    merged tasks more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {:id})
+      params.require(:user).permit(:id)
     end
 end
