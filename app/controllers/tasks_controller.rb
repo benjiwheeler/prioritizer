e77b35544b2f5d8dcc1f234dcea30bf222a3560f
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-07-21 14:41:46 -0400

    ability to add new task and return to next

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to :next, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
