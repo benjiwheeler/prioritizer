40d8059cf49e0c6071567f22939e0a193aced5cb
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-07-21 13:47:53 -0400

    trying to debug sort order

--- app/models/user.rb
+++ app/models/user.rb
@@ -144,6 +144,7 @@ class User < ActiveRecord::Base
     sorted_tasks.sort_by do |task|
       task.get_importance! #+ Task.random_score
     end.reverse!
+    Rails.logger.warn("after sorting, order is: #{sorted_tasks")
     sorted_tasks.each do |task|
       Rails.logger.warn("redis adding task #{task.id} to #{self.redis_user_tag_tasks_key(tag_str)}")
       $redis.rpush(self.redis_user_tag_tasks_key(tag_str), task.id);
