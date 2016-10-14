dbed13f8cd13c0d59a65da1b7a2eeee4c4f72c1b
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-10-14 14:38:52 -0400

    added mailer lines i mistakenly put in technosocial instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #  added per Devise instructions
+  config.action_mailer.default_url_options = { host: 'prioritizershake.herokuapp.com' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
