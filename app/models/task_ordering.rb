29bf078f424178e92f2a09a7ddd5659ba7b3f7ca
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-12-20 09:01:22 -0500

    cleaned up task_ordering a bit

--- app/models/task_ordering.rb
+++ app/models/task_ordering.rb
@@ -1,44 +1,54 @@
 class TaskOrdering
-
-  def TaskOrdering.redis_tasks_key_prefix(user)
-    user.redis_key + "::tasks"
+  def self.redis_tasks_key_prefix(user)
+    user.redis_key + '::tasks'
   end
 
-  def TaskOrdering.USER_TASKS_REDIS_VALID_SECS
+  def self.USER_TASKS_REDIS_VALID_SECS
     60 * 60
   end
 
-  def TaskOrdering.redis_user_tag_tasks_key(user, tag_str)
-    #tag_str = "all" if tag_str.blank?
-    TaskOrdering.redis_tasks_key_prefix(user) + "::tag:" + tag_str.to_s + "::task_ids"
+  def self.redis_user_tag_tasks_key(user, tag_str)
+    TaskOrdering.redis_tasks_key_prefix(user) +
+      '::tag:' + tag_str.to_s + '::task_ids'
   end
 
-  def TaskOrdering.expire_redis_tasks_keys!(user)
+  def self.expire_redis_tasks_keys!(user)
     keys = $redis.keys "#{redis_tasks_key_prefix(user)}*"
     $redis.del(*keys) unless keys.empty?
   end
 
+  def self.order_by_ease(unsorted_tasks)
+    unsorted_tasks.sort do |task_a, task_b|
+      task_b.get_overall_ease! * 4 +
+        task_b.get_overall_imp_alone! * 2 +
+        task_b.get_session_rand! \
+        <=> \
+        task_a.get_overall_ease! * 4 +
+        task_a.get_overall_imp_alone! * 2 +
+        task_a.get_session_rand!
+    end
+  end
+
   # truth it it's not JUST by ease, it also considers importance, timing and random factor.
   # but ease is heavily weighted.
-  def TaskOrdering.tasks_ordered_by_ease!(user, tag_str = nil)
-    sorted_tasks = user.tasks.where(done: false).includes(:tags)
-    if tag_str.present? && tag_str != "all" # "all" is the same as no tag!
-      sorted_tasks = sorted_tasks.where(done: false).tagged_with([tag_str, "all"], :any => true).includes(:tags) # "all" is always ok!
+  def self.tasks_ordered_by_ease!(user, tag_str = nil)
+    unsorted_tasks = user.tasks.where(done: false).includes(:tags)
+    if tag_str.present? && tag_str != 'all' # 'all' is the same as no tag!
+      unsorted_tasks = unsorted_tasks.where(done: false)
+                                     .tagged_with([tag_str, 'all'], any: true)
+                                     .includes(:tags)
+      # 'all' is always ok!
     end
 
     # sort with best first, worst last
-    sorted_tasks = sorted_tasks.sort do |taskA, taskB|
-      taskB.get_overall_ease! * 4 + taskB.get_overall_imp_alone! * 2 + taskB.get_session_rand! \
-      <=> \
-      taskA.get_overall_ease! * 4 + taskA.get_overall_imp_alone! * 2 + taskA.get_session_rand!
-    end
+    sorted_tasks = TaskOrdering.order_by_ease(unsorted_tasks)
     Rails.logger.warn("after ease sorting, order is: #{sorted_tasks.to_json}")
-    return sorted_tasks
+    sorted_tasks
   end
 
   # sorts based on vital+immediate, with bonuses for several tasks that are especially low in heavy+long.
   # stores the resulting ordering in redis.
-  def TaskOrdering.generate_overall_ordered_tasks!(user, tag_str = nil)
+  def self.generate_overall_ordered_tasks!(user, tag_str = nil)
     #
     # set ease bonus
     #
@@ -46,7 +56,9 @@ class TaskOrdering
     tasks_by_ease = TaskOrdering.tasks_ordered_by_ease!(user, tag_str)
     by_ease_num_divisions = 8 # like a quartile or quintile
     by_ease_num_to_give_bonus = 3 # out of by_ease_num_divisions
-    by_ease_num_to_give_bonus = by_ease_num_divisions if by_ease_num_to_give_bonus > by_ease_num_divisions
+    if by_ease_num_to_give_bonus > by_ease_num_divisions
+      by_ease_num_to_give_bonus = by_ease_num_divisions
+    end
     by_ease_count_per_division = (tasks_by_ease.count / (by_ease_num_divisions + 0.000001)).to_i
     by_ease_first_division_bonus = 1.0
     by_ease_last_division_bonus = 0.0
@@ -70,8 +82,8 @@ class TaskOrdering
     # get complete ordering with all factors considered
     #
     unsorted_tasks = user.tasks.where(done: false).includes(:tags)
-    if tag_str.present? && tag_str != "all" # "all" is the same as no tag!
-      unsorted_tasks = unsorted_tasks.tagged_with([tag_str, "all"], :any => true) # "all" is always ok!
+    if tag_str.present? && tag_str != 'all' # 'all' is the same as no tag!
+      unsorted_tasks = unsorted_tasks.where(done: false).tagged_with([tag_str, 'all'], :any => true) # 'all' is always ok!
     end
     unsorted_tasks.each do |task|
       if task_bonuses.has_key?(task.id)
@@ -96,7 +108,7 @@ class TaskOrdering
     return sorted_tasks.map{|task| task.id}
   end
 
-  def TaskOrdering.n_ordered_tasks!(user, tag_str = nil, n = :all)
+  def self.n_ordered_tasks!(user, tag_str = nil, n = :all)
     # get the entire ordered list from Redis
     cached_ordered_task_ids = $redis.lrange(TaskOrdering.redis_user_tag_tasks_key(user, tag_str), 0, -1)
     if cached_ordered_task_ids.blank?
@@ -128,7 +140,7 @@ class TaskOrdering
     return n_ordered_tasks
   end
 
-  def TaskOrdering.first_ordered_task!(user, tag_str = nil)
+  def self.first_ordered_task!(user, tag_str = nil)
     one_ordered_task_array = TaskOrdering.n_ordered_tasks!(user, tag_str, 1)
     if one_ordered_task_array.blank?
       Rails.logger.warn("user:first_ordered_task!: no tasks for user #{user}")
@@ -139,7 +151,7 @@ class TaskOrdering
   end
 
   # NOTE: need to integrate this logic more smartly
-  def TaskOrdering.get_next_task!(user, tag_str = nil)
+  def self.get_next_task!(user, tag_str = nil)
     first_ordered_task = TaskOrdering.first_ordered_task!(user, tag_str)
     if first_ordered_task.present?
       return first_ordered_task.first_youngest_descendent({done: false})
@@ -149,6 +161,5 @@ class TaskOrdering
       return nil
     end
   end
-
 
 end
